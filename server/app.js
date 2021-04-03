a7bdbc5a1bec1db9132bea3d9bde352374c04f71
Kiptoo Biwot <elviskb@gmail.com>
2021-04-03 17:27:51 +0300

    Error Handling & DB Connection

--- server/app.js
+++ server/app.js
@@ -1,11 +1,22 @@
-const express     = require('express')
-const mongoose    = require('mongoose')
-const morgan      = require('morgan')
-const cors        = require('cors')
+const express       = require('express')
+const mongoose      = require('mongoose')
+const morgan        = require('morgan')
+const cors          = require('cors')
+const createError = require('http-errors')
+require('dotenv').config()
 
 const PORT = 3000
 
 const app = express()
+
+// DB Connection
+mongoose.connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+}, () => {
+    console.log('Cloud Database Connected...')
+})
 
 // Middleware
 app.use(express.json())
@@ -18,6 +29,20 @@ app.get('/', (req, res) => {
     res.send({message: 'Welcome to Lima Smart server'})
 })
 
+// Error handling
+app.use(async (req, res, next) => {
+    next(createError.NotFound())
+})
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500)
+    res.send({
+        error: {
+            status: err.status || 500,
+            message: err.message
+        }
+    })
+})
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server is listening on port ${PORT}`)
